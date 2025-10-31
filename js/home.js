3b0196ff468cdaf1d813d6350e7d67d35065e29e
Douglas Castro <69062335+DouglasCastroSousa@users.noreply.github.com>
2025-10-30 22:27:52 -0300

    Adicionando API de tradução para a mensagem de conselho

--- js/home.js
+++ js/home.js
@@ -41,15 +41,26 @@ new Chart(DIV_GRAF1, {
   });
 
 // adicionando API ao meu dashboard
-async function loadmessage(){
-  const mensagem = document.getElementById("boas_vindas");
-  const url = 'https://api.adviceslip.com/advice';
+async function loading_message() {
+    const texto = document.getElementById("boas_vindas");
+    const advice_url = 'https://api.adviceslip.com/advice';
 
-  const response = await fetch(url);
-  const datas = await response.json();
+    const resposta = await fetch(advice_url);
+    const dados = await resposta.json();
 
-  mensagem.textContent = datas.slip.advice; // coloca o conselho no HTML
+    const mensagem_em_ingles = dados.slip.advice;
+    
+    const texto_para_url = encodeURIComponent(mensagem_em_ingles)
+    const lingua_origem = "en";
+    const lingua_objetivo = "pt";
 
+    const translate_url = `https://api.mymemory.translated.net/get?q=${texto_para_url}&langpair=${lingua_origem}|${lingua_objetivo}`;
+
+    const resposta_trad = await fetch(translate_url);
+    const dados_trad = await resposta_trad.json();
+
+    texto.textContent = (dados_trad.responseData.translatedText);
 }
 
-loadmessage();
+loading_message();
+
