257bf45361d2812bb4a225f53386ebc48a0cbe2f
Diego Castro <diegocastro1087@gmail.com>
2025-11-15 17:57:32 -0300

    nova validação de Login, com Bootstrap

--- js/login.js
+++ js/login.js
@@ -1,64 +1,19 @@
-const form = document.getElementById('form_login')
-const email = document.getElementById('email')
-const senha = document.getElementById('senha')
-const validacao = document.getElementById('validar_erro')
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  'use strict'
 
-form.addEventListener('submit', (e) => {
-    let valido = true
-    validacao.innerHTML = '';
-    let mensagens = ''
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
 
-    if (email.value.trim() == '') {
-        e.preventDefault()
-        mensagens += '<div class="text-danger mb-1">• Preencha o e-mail<br></div>'
-        valido = false
-    }else{
-        mensagens += '<div class="text-success mb-1">• Preencha o e-mail<br></div>'
-    }
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
 
-    if (senha.value == '') {
-        e.preventDefault()
-        mensagens += '<div class="text-danger mb-1">• Preencha a senha<br></div>'
-        valido = false
-    }else{
-        mensagens += '<div class="text-success mb-1">• Preencha a senha<br></div>'
-    }
-
-    if (!email.checkValidity()) {
-        e.preventDefault()
-        mensagens += '<div class="text-danger mb-1">• Formato do e-mail inválido (Exemplo@gmail.com)<br></div>'
-        email.style.borderColor = 'red'
-        email.style.borderWidth = '3px'
-        valido = false
-    } else {
-        email.style.borderColor = 'green'
-        email.style.borderWidth = '3px'
-        mensagens += '<div class="text-success mb-1">• Formato do e-mail inválido (Exemplo@gmail.com)<br></div>'
-    }
-
-    if (!senha.checkValidity()) {
-        e.preventDefault()
-        mensagens += '<div class="text-danger mb-1">• Formato da senha inválido (Conter pelo menos 6 digitos)<br></div>'
-        senha.style.borderColor = 'red'
-        senha.style.borderWidth = '3px'
-        valido = false
-    } else {
-        senha.style.borderColor = 'green'
-        senha.style.borderWidth = '3px'
-        mensagens += '<div class="text-success mb-1">• Formato da senha inválido (Conter pelo menos 6 digitos)<br></div>'
-    }
-
-    if (valido) {
-
-        if (email.value == 'pessoa@gmail.com' && senha.value == '123456') {
-            alert('Login realizado')
-        } else {
-            e.preventDefault()
-            validacao.innerHTML = '<div class="text-success mb-1">• Preencha o e-mail<br></div><div class="text-success mb-1">• Preencha a senha<br></div><div class="text-success mb-1">• Formato do e-mail inválido (Exemplo@gmail.com)<br></div><div class="text-success mb-1">• Formato da senha inválido (Conter pelo menos 6 digitos)<br></div>';
-            validacao.innerHTML += '<div class="text-danger mb-1">• email ou senha de login incorretos</div>'
-        }
-    } else {
-        validacao.innerHTML = mensagens;
-        return;
-    }
-})
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
