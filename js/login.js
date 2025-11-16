6151782d50e6db2a0c4aaf8236045f0da0687c2d
Diego Castro <diegocastro1087@gmail.com>
2025-11-16 14:47:56 -0300

    atualizando validação de formulario(agora mostra os erros)

--- js/login.js
+++ js/login.js
@@ -6,6 +6,20 @@
       if (!form.checkValidity()) {
         event.preventDefault()
         event.stopPropagation()
+
+        Array.from(form.elements).forEach(input => {
+          if(input.tagName !== 'INPUT' || !input.required) return;
+
+          if(!input.validity.valid && input.id == 'email'){
+            const error_message = input.validationMessage
+            document.getElementById('email-invalido').innerHTML = error_message;
+          }else if (!input.validity.valid && input.id == 'senha'){
+            const error_message = input.validationMessage
+            document.getElementById('senha-invalida').innerHTML = error_message;
+          }
+          
+        })
+
       }
       form.classList.add('was-validated')
     }, false)
